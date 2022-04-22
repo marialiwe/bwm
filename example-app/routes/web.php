407ac840b70b8a53895f21ceefde438b606f1e74
maria <maria.422020025@civitas.ukrida.ac.id>
2022-04-22 10:59:09 +0700

    Install breeze & install & integration socialite

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('success-checkout', function () {
+    return view('success_checkout');
+})->name('success-checkout');
+
+//socialite routes
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
