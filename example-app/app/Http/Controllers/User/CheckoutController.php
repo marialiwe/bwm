10de44fe67698eb7505671a2b8ddfc9041f2e997
maria <maria.422020025@civitas.ukrida.ac.id>
2022-05-13 01:02:02 +0700

    feat: integrate discount with checkout page

--- example-app/app/Http/Controllers/User/CheckoutController.php
+++ example-app/app/Http/Controllers/User/CheckoutController.php
@@ -14,9 +14,9 @@ use Mail;
 use Str;
 use Midtrans;
 
-
 class CheckoutController extends Controller
 {
+
     public function __construct()
     {
         Midtrans\Config::$serverKey = env('MIDTRANS_SERVERKEY');
@@ -24,7 +24,6 @@ class CheckoutController extends Controller
         Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
         Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
     }
-
 
     /**
      * Display a listing of the resource.
@@ -67,12 +66,19 @@ class CheckoutController extends Controller
 
         // update user data
         $user = Auth::user();
-        $user -> email = $data['email'];
-        $user -> name = $data['name'];
-        $user -> occupation = $data['occupation'];
+        $user->email = $data['email'];
+        $user->name = $data['name'];
+        $user->occupation = $data['occupation'];
         $user->phone = $data['phone'];
         $user->address = $data['address'];
-        $user -> save();
+        $user->save();
+
+        // checkout discount
+        if ($request->discount) {
+            $discount = Discount::whereCode($request->discount)->first();
+            $data['discount_id'] = $discount->id;
+            $data['discount_percentage'] = $discount->percentage;
+        }
 
         // create checkout
         $checkout = Checkout::create($data);
@@ -128,10 +134,12 @@ class CheckoutController extends Controller
     {
         //
     }
+
     public function success()
     {
         return view('checkout.success');
     }
+
     /**
      * Midtrans Handler
      */
@@ -142,16 +150,28 @@ class CheckoutController extends Controller
 
         $checkout->midtrans_booking_code = $orderId;
 
-        $transaction_details = [
-            'order_id' => $orderId,
-            'gross_amount' => $price
-        ];
-
         $item_details[] = [
             'id' => $orderId,
             'price' => $price,
             'quantity' => 1,
             'name' => "Payment for {$checkout->Camp->title} Camp"
+        ];
+
+        $discountPrice = 0;
+        if ($checkout->Discount) {
+            $discountPrice = $price * $checkout->discount_percentage / 100;
+            $item_details[] = [
+                'id' => $checkout->Discount->code,
+                'price' => -$discountPrice,
+                'quantity' => 1,
+                'name' => "Discount {$checkout->Discount->name} ({$checkout->discount_percentage}%)"
+            ];
+        }
+
+        $total = $price - $discountPrice;
+        $transaction_details = [
+            'order_id' => $orderId,
+            'gross_amount' => $total
         ];
 
         $userData = [
@@ -183,6 +203,7 @@ class CheckoutController extends Controller
             // Get Snap Payment Page URL
             $paymentUrl = \Midtrans\Snap::createTransaction($midtrans_params)->redirect_url;
             $checkout->midtrans_url = $paymentUrl;
+            $checkout->total = $total;
             $checkout->save();
 
             return $paymentUrl;
@@ -194,7 +215,6 @@ class CheckoutController extends Controller
     public function midtransCallback(Request $request)
     {
         $notif = $request->method() == 'POST' ? new Midtrans\Notification() : Midtrans\Transaction::status($request->order_id);
-
 
         $transaction_status = $notif->transaction_status;
         $fraud = $notif->fraud_status;
